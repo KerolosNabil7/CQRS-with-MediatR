5a3e93ce3db3aee4e1c55fd6b326ac1caa153d40
KerolosNabil7 <kerolosnabil926@gmail.com>
2025-04-14 15:31:35 +0200

    Add GetProduct Feature

---
+++ CQRS with MediatR/Features/Products/Queries/Get/GetProductQuery.cs
@@ -0,0 +1,7 @@
+﻿using CQRS_with_MediatR.Features.Products.Dtos;
+using MediatR;
+
+namespace CQRS_with_MediatR.Features.Products.Queries.Get
+{
+    public record GetProductQuery(Guid Id) : IRequest<ProductDto>;
+}
---
+++ CQRS with MediatR/Features/Products/Queries/Get/GetProductQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using CQRS_with_MediatR.Features.Products.Dtos;
+using CQRS_with_MediatR.Persistence;
+using MediatR;
+
+namespace CQRS_with_MediatR.Features.Products.Queries.Get
+{
+    public class GetProductQueryHandler : IRequestHandler<GetProductQuery, ProductDto?>
+    {
+        private readonly AppDbContext _context;
+        public GetProductQueryHandler(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ProductDto?> Handle(GetProductQuery request, CancellationToken cancellationToken)
+        {
+            var product = await _context.Products.FindAsync(request.Id);
+            if (product == null)
+            {
+                return null;
+            }
+            return new ProductDto(product.Id, product.Name, product.Description, product.Price);
+        }
+    }
+}
