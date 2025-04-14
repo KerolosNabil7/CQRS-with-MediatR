2ee905d610063af52f03e79dce5c456bc97a6f06
KerolosNabil7 <kerolosnabil926@gmail.com>
2025-04-14 11:12:22 +0200

    Create the AppDbContext class, seed data and register the DbContext to the DI Container in Program.cs file

---
+++ CQRS with MediatR/Persistence/AppDbContext.cs
@@ -0,0 +1,33 @@
+﻿using CQRS_with_MediatR.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace CQRS_with_MediatR.Persistence
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        {
+            //Ensuring the database is created
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Define the primary key for the Product because the EF Core will not catch it since it GUID not int
+            modelBuilder.Entity<Product>().HasKey(p=>p.Id);
+
+            //seed the table with initial records
+            modelBuilder.Entity<Product>().HasData(
+            new Product("iPhone 15 Pro", "Apple's latest flagship smartphone with a ProMotion display and improved cameras", 999.99m),
+            new Product("Dell XPS 15", "Dell's high-performance laptop with a 4K InfinityEdge display", 1899.99m),
+            new Product("Sony WH-1000XM4", "Sony's top-of-the-line wireless noise-canceling headphones", 349.99m)
+        );
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase("InMemoryDB");
+        }
+    }
+}
