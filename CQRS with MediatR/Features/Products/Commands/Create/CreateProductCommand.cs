a2805c7ade3859553042b1df9c211285f390465a
KerolosNabil7 <kerolosnabil926@gmail.com>
2025-04-14 15:37:45 +0200

    Add CreateProduct Command Feature

---
+++ CQRS with MediatR/Features/Products/Commands/Create/CreateProductCommand.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+
+namespace CQRS_with_MediatR.Features.Products.Commands.Create
+{
+    public record CreateProductCommand(string Name, string Description, decimal Price) : IRequest<Guid>;
+}
---
+++ CQRS with MediatR/Features/Products/Commands/Create/CreateProductCommandHandler.cs
@@ -0,0 +1,23 @@
+﻿using CQRS_with_MediatR.Domain;
+using CQRS_with_MediatR.Persistence;
+using MediatR;
+
+namespace CQRS_with_MediatR.Features.Products.Commands.Create
+{
+    public class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, Guid>
+    {
+        private readonly AppDbContext _context;
+        public CreateProductCommandHandler(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Guid> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            var product = new Product(command.Name, command.Description, command.Price);
+            await _context.AddAsync(product);
+            await _context.SaveChangesAsync();
+            return product.Id;
+        }
+    }
+}
