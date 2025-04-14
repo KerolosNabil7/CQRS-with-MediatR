593d6b6fb3e043859107b028915c0d4c7c0339bf
KerolosNabil7 <kerolosnabil926@gmail.com>
2025-04-14 15:08:46 +0200

    Implement the List Feature

---
+++ CQRS with MediatR/Features/Products/Queries/List/ListProductsQuery.cs
@@ -0,0 +1,9 @@
+﻿using CQRS_with_MediatR.Features.Products.Dtos;
+using MediatR;
+
+namespace CQRS_with_MediatR.Features.Products.Queries.List
+{
+    //Every Query / Command object would inherit from IRequest<T> interface of the MediatR library, where T is the object to be returned.
+    //so it will be IRequest< List<ProductDto> >
+    public record ListProductsQuery : IRequest<List<ProductDto>>;
+}
---
+++ CQRS with MediatR/Features/Products/Queries/List/ListProductsQueryHandler.cs
@@ -0,0 +1,23 @@
+﻿using CQRS_with_MediatR.Features.Products.Dtos;
+using CQRS_with_MediatR.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace CQRS_with_MediatR.Features.Products.Queries.List
+{
+    //Also, all the handlers would implement the IRequestHandler<T, R> where T is the incoming request (which in our case would be the Query itself), and R would be the response, which is a list of products.
+    public class ListProductsQueryHandler : IRequestHandler<ListProductsQuery, List<ProductDto>>
+    {
+        private readonly AppDbContext _context;
+        public ListProductsQueryHandler(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<ProductDto>> Handle(ListProductsQuery request, CancellationToken cancellationToken)
+        {
+            var products = await _context.Products.Select(p => new ProductDto(p.Id, p.Name, p.Description, p.Price)).ToListAsync();
+            return products;
+        }
+    }
+}
