0a814f00492824eb087dea3086c9d91df5cf1844
KerolosNabil7 <kerolosnabil926@gmail.com>
2025-04-14 11:48:57 +0200

    Create a ProductDto as Record not DTO class

--- CQRS with MediatR/Features/Products/Dtos/ProductDto.cs
+++ CQRS with MediatR/Features/Products/Dtos/ProductDto.cs
@@ -1,7 +1,4 @@
 ﻿namespace CQRS_with_MediatR.Features.Products.Dtos
 {
-    public class ProductDto
-    {
-
-    }
+    public record ProductDto(Guid Id, string Name, string Description, decimal Price);
 }
